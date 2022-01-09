ed658fb719429ba218050daa70d4ea72beb2e4c3
Ashley Strahle <a.strahle@me.com>
2022-01-09 15:34:28 +1000

    async transform

--- MyGL/ETL.cs
+++ MyGL/ETL.cs
@@ -19,20 +19,21 @@ namespace MyGL.Controllers
         {
             foreach (var record in _context.LoadTable)
             {
-                Transaction transaction = new Transaction()
+                // Only add transaction if it doesn't already exist
+                if (_context.Transactions.Where(t => t.Date == record.Date && t.Description == record.Description && t.Amount == record.Amount && t.AccountId == record.AccountId && t.Balance == record.Balance).Count() == 0)
                 {
-                    Date = record.Date,
-                    Description = record.Description,
-                    Amount = record.Amount,
-                    AccountId = record.AccountId,
-                    Balance = record.Balance,
-                    Debit = record.Amount < 0 ? record.Amount : 0,
-                    DebitAmount = record.Amount < 0 ? 0 - record.Amount : 0,
-                    Credit = record.Amount > 0 ? record.Amount : 0,
-                    GST = record.Amount / 11
-                };
-                if (_context.Transactions.Where(t => t.Date == transaction.Date && t.Description == transaction.Description && t.Amount == transaction.Amount && t.AccountId == transaction.AccountId).Count() == 0)
-                {
+                    Transaction transaction = new Transaction()
+                    {
+                        Date = record.Date,
+                        Description = record.Description,
+                        Amount = record.Amount,
+                        AccountId = record.AccountId,
+                        Balance = record.Balance,
+                        Debit = record.Amount < 0 ? record.Amount : 0,
+                        DebitAmount = record.Amount < 0 ? 0 - record.Amount : 0,
+                        Credit = record.Amount > 0 ? record.Amount : 0,
+                        GST = record.Amount / 11
+                    };
                     _context.Transactions.Add(transaction);
                 }
             }
@@ -41,20 +42,43 @@ namespace MyGL.Controllers
 
         public async Task Transform()
         {
-            foreach (var transaction in _context.Transactions.Where(t => t.CategoryId == null))
+            // Get all uncategorized transactions
+            List<Transaction> transactions = _context.Transactions.Where(t => t.CategoryId == null).ToList();
+
+            // Break transactions up into groups of 50
+            int GroupSize = 50;
+            List<List<Transaction>> TransactionGroup = new List<List<Transaction>>();
+            while (transactions.Count > 0)
+            {
+                int count = transactions.Count > GroupSize ? GroupSize : transactions.Count;
+                TransactionGroup.Add(transactions.GetRange(0, count));
+                transactions.RemoveRange(0, count);
+            }
+
+            // Asynchronously process transaction groups
+            var tasks = TransactionGroup.Select(async transgroup =>
+            {
+                Categorise(transgroup);
+            });
+
+            // Wait for all tasks to complete
+            await Task.WhenAll(tasks);
+            _context.SaveChanges();
+        }
+
+        public async Task Categorise(List<Transaction> transactions)
+        {
+            foreach (Transaction transaction in transactions)
             {
                 foreach (var category in _context.CategoryCondition)
                 {
                     if (transaction.Description.Contains(category.SearchString))
                     {
-                        Transaction update_trans = _context.Transactions.SingleOrDefault(t => t.Id == transaction.Id);
-                        update_trans.CategoryId = category.CategoryId;
-                        _context.Update(update_trans);
-                        _context.Attach(update_trans).State = EntityState.Modified;
+                        transaction.CategoryId = category.CategoryId;
+                        _context.Attach(transaction).State = EntityState.Modified;
                     }
                 }
             }
-            _context.SaveChanges();
         }
     }
 }
