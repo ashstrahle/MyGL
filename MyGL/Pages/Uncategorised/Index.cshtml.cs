40ec71b1316b5e7ca5bdf5263bd1035c20d030fe
Ashley Strahle <a.strahle@me.com>
2022-01-09 16:12:53 +1000

    Uncategorised

---
+++ MyGL/Pages/Uncategorised/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿#nullable disable
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyGL.Models;
+
+namespace MyGL.Pages.Uncategorised
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyGL.Data.MyGLContext _context;
+
+        public IndexModel(MyGL.Data.MyGLContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Transaction> Transactions { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Transactions = await _context.Transactions.Where(t => t.CategoryId == null).OrderBy(t => t.Date).ToListAsync();
+        }
+    }
+}
