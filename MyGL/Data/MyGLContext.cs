951bc0ec79d8dfd43699f19d1fc4ba90b9ace965
Ashley Strahle <a.strahle@me.com>
2022-01-14 00:36:27 +1000

    Pivot

--- MyGL/Data/MyGLContext.cs
+++ MyGL/Data/MyGLContext.cs
@@ -16,5 +16,6 @@ namespace MyGL.Data
         public DbSet<MyGL.Models.LoadTable> LoadTable { get; set; }
         public DbSet<MyGL.Models.Transaction> Transactions { get; set; }
         public DbSet<MyGL.Models.DimDate> DimDates { get; set; }
+        public DbSet<MyGL.Models.PivotData> View_PivotData { get; set; }
     }
 }
