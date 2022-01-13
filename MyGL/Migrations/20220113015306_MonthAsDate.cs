ad35cd5c79017dd82d590002f152fcfe83f06bc9
Ashley Strahle <a.strahle@me.com>
2022-01-13 12:35:30 +1000

    Month As Date

---
+++ MyGL/Migrations/20220113015306_MonthAsDate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyGL.Migrations
+{
+    public partial class MonthAsDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "MonthAsDate",
+                table: "Transactions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MonthAsDate",
+                table: "Transactions");
+        }
+    }
+}
