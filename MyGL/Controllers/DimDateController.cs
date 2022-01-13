7c117d0a8a672e9e451c38f20619433f335ec804
Ashley Strahle <a.strahle@me.com>
2022-01-13 10:29:50 +1000

    DimDate

--- MyGL/Controllers/DimDateController.cs
+++ MyGL/Controllers/DimDateController.cs
@@ -22,39 +22,42 @@ namespace MyGL
         public void Generate(DateTime StartDate, DateTime FinishDate)
         {
             // Clear DimDates
-            _context.DimDates.RemoveRange(_context.DimDates);
-            _context.SaveChanges();
+            // _context.DimDates.RemoveRange(_context.DimDates);
+            // _context.SaveChanges();
 
             DateTime CurrentDate = StartDate;
 
             while (CurrentDate <= FinishDate)
             {
-                DimDate dimDate = new()
+                if (_context.DimDates.Where(d => d.Date == CurrentDate).Count() == 0)
                 {
-                    DateId = Int32.Parse(CurrentDate.Year.ToString("D4") + CurrentDate.Month.ToString("D2") + CurrentDate.Day.ToString("D2")),
-                    Date = CurrentDate,
-                    Year = Int32.Parse(CurrentDate.Year.ToString("D4")),
-                    Month = CurrentDate.Month,
-                    Day = CurrentDate.Day,
-                    MonthName = CurrentDate.ToString("MMMM"),
-                    MonthNameShort = CurrentDate.ToString("MMM"),
-                    WeekDay = CurrentDate.ToString("dddd"),
-                    WeekDayShort = CurrentDate.ToString("ddd"),
-                    DayOfWeek = (int)(((int)CurrentDate.DayOfWeek + 6) % 7 + 1),
-                    Quarter = (int)((CurrentDate.Month + 2) / 3),
-                    QuarterFormat = "Q" + ((int)(CurrentDate.Month + 2) / 3),
-                    DayOfYear = CurrentDate.DayOfYear,
-                    WeekNumber = (int)((CurrentDate.DayOfYear / 7) + 1),
-                    WeekNumberFormat = "W" + (int)((CurrentDate.DayOfYear / 7) + 1),
-                    MonthNameFormat = CurrentDate.Month.ToString("D2") + " - " + CurrentDate.ToString("MMMM"),
-                    MonthNameShortFormat = CurrentDate.Month.ToString("D2") + " - " + CurrentDate.ToString("MMM"),
-                    FinancialYear = (CurrentDate.Month < 7 ? (int)(CurrentDate.Year - 1) + "/" + CurrentDate.Year : CurrentDate.Year + "/" + (int)(CurrentDate.Year + 1)),
-                    FinancialQuarter = (int)(((CurrentDate.Month + 5) % 12) / 3) + 1,
-                    FinancialQuarterFormat = "Q" + (int)((((CurrentDate.Month + 5) % 12) / 3) + 1),
-                    FinancialQuarterMonthFormat = "Q" + (int)((((CurrentDate.Month + 5) % 12) / 3) + 1) + "/" + CurrentDate.Month.ToString("D2") + " - " + CurrentDate.ToString("MMMM"),
-                    FinancialQuarterMonthShortFormat = "Q" + (int)((((CurrentDate.Month + 5) % 12) / 3) + 1) + "/" + CurrentDate.Month.ToString("D2") + " - " + CurrentDate.ToString("MMM")
-                };
-                _context.Add(dimDate);
+                    DimDate dimDate = new()
+                    {
+                        DateId = Int32.Parse(CurrentDate.Year.ToString("D4") + CurrentDate.Month.ToString("D2") + CurrentDate.Day.ToString("D2")),
+                        Date = CurrentDate,
+                        Year = Int32.Parse(CurrentDate.Year.ToString("D4")),
+                        Month = CurrentDate.Month,
+                        Day = CurrentDate.Day,
+                        MonthName = CurrentDate.ToString("MMMM"),
+                        MonthNameShort = CurrentDate.ToString("MMM"),
+                        WeekDay = CurrentDate.ToString("dddd"),
+                        WeekDayShort = CurrentDate.ToString("ddd"),
+                        DayOfWeek = (int)(((int)CurrentDate.DayOfWeek + 6) % 7 + 1),
+                        Quarter = (int)((CurrentDate.Month + 2) / 3),
+                        QuarterFormat = "Q" + ((int)(CurrentDate.Month + 2) / 3),
+                        DayOfYear = CurrentDate.DayOfYear,
+                        WeekNumber = (int)((CurrentDate.DayOfYear / 7) + 1),
+                        WeekNumberFormat = "W" + (int)((CurrentDate.DayOfYear / 7) + 1),
+                        MonthNameFormat = CurrentDate.Month.ToString("D2") + " - " + CurrentDate.ToString("MMMM"),
+                        MonthNameShortFormat = CurrentDate.Month.ToString("D2") + " - " + CurrentDate.ToString("MMM"),
+                        FinancialYear = (CurrentDate.Month < 7 ? (int)(CurrentDate.Year - 1) + "/" + CurrentDate.Year : CurrentDate.Year + "/" + (int)(CurrentDate.Year + 1)),
+                        FinancialQuarter = (int)(((CurrentDate.Month + 5) % 12) / 3) + 1,
+                        FinancialQuarterFormat = "Q" + (int)((((CurrentDate.Month + 5) % 12) / 3) + 1),
+                        FinancialQuarterMonthFormat = "Q" + (int)((((CurrentDate.Month + 5) % 12) / 3) + 1) + "/" + CurrentDate.Month.ToString("D2") + " - " + CurrentDate.ToString("MMMM"),
+                        FinancialQuarterMonthShortFormat = "Q" + (int)((((CurrentDate.Month + 5) % 12) / 3) + 1) + "/" + CurrentDate.Month.ToString("D2") + " - " + CurrentDate.ToString("MMM")
+                    };
+                    _context.Update(dimDate);
+                }
                 CurrentDate = CurrentDate.AddDays(1);
             }
             _context.SaveChanges();
